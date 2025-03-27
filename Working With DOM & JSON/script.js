9f91e6480a96d5bb10753c5b18b34dec74973c68
vishva642 <vishvapatel764@gmail.com>
2025-03-27 11:23:13 +0530

    commiting code

---
+++ Working With DOM & JSON/script.js
@@ -0,0 +1,14 @@
+// let person = {
+//     "name" : "Vishva",
+//     "age" : 18,
+//     "id" : 8872
+// }
+// localStorage.setItem("user",JSON.stringify(person))
+// let obj = JSON.parse(localStorage.getItem("user"))
+// console.log(obj)
+// console.log(typeof obj)
+// console.log(person.name)
+
+// const firstPrise = "Sejal"
+
+// localStorage.setItem("p1", firstPrise);
